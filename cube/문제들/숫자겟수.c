f1a25ce876ffdc1f963b81c26a49f0cb9b93507f
NamHyeok-Kim <cubecube06@gmail.com>
2020-07-25 08:35:31 +0900

    .

---
+++ cube/문제들/숫자겟수.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	freopen("input.txt", "r", stdin);
+
+	int A, B, C;
+	char x;
+
+	scanf("%d %d %d", A, B, C);
+
+	x = A * B * C;
+
+	for(int i = 0; i < 10; i++ )
+
+}
