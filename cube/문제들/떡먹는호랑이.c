e622f3ad1ca280af7d64ea3655dd335a103c2276
eugene <cubecube06@gmail.com>
2020-07-25 09:09:57 +0900

    push

--- cube/문제들/떡먹는호랑이.c
+++ cube/문제들/떡먹는호랑이.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-fibo(int day, int dd, int o, int t)
+int fibo(int day, int dd, int o, int t)
 {
 	int tmp = 0;
 	for (int i = 0; i < day; i++) {
-		tmp = t;
+		tmp = o;
 		o = t;
-		t = tmp;
+		t = tmp + t;
 	}
 	if (tmp == dd) {
 		return 0;
@@ -18,15 +18,16 @@ int main()
 {
 	freopen("input.txt", "r", stdin);
 
-	int a, b;
+	int a, b, j;
 	scanf("%d %d", &a, &b);
 
-	for (int i = 0; i < a; i++) {
-		for (int j = i; j < a; j++) {
+	for (int i = 1; i < 100; i++) {
+		for (j = i; j < 100; j++) {
 			if (fibo(a, b, i, j) == 0) {
 				printf("%d %d", i, j);
 				break;
 			}
 		}
+		if (j != 100) break;
 	}
 }
