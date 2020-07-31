2aae3d352bcd699c78fdb421b297e116cef3deaf
NamHyeok-Kim <cubecube06@gmail.com>
2020-07-31 23:22:14 +0900

    이에에에에ㅔ

--- cube/문제들/줄세우기.cpp
+++ cube/문제들/줄세우기.cpp
@@ -14,35 +14,23 @@ int main()
 	fin >> s;
 
 	vector<int> N(s);
-	
-	//첫번째 줄은 인원수
-	//두번째 줄은 서로간의 번호차
-	//자료를 먼저 읽어오도록 한다.
 
-
-
-	//입력된 값을 기준으로 실제 줄세우기 배열의 최종 번호를 결정한다.
-
-
-
-	//결과를 출력한다.
-
+	for(int i = 0; i < s; i++) N[i] = i + 1;
 
 	for (int i = 0; i < s; i++) {//5
-		N[i] = i + 1;
 		int ticket;
 		fin >> ticket;
 		for (int j = 0; j < ticket; j++) {//6
-			tmp = N[i];
-			N[i] = N[i + 1];
-			N[i + 1] = tmp;
+			int tmp = N[i];
+			N[i] = N[j + i - ticket ];
+			N[j + i - ticket ] = tmp;
 		}
 		
 	}
 	cout << endl;
 
 	
-	for (int i = 0; i < N.size(); i++){
+	for (int i = 0; i < s; i++){
 		cout << N[i] << " ";
 	}
 }
